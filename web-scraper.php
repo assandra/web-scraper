eaf53cf5d733104e38f240d76992801f5c8236f8
assandra <assandrafalls1@gmail.com>
2021-02-09 13:12:54 +0000

    Handle case when no response from  curl request

--- web-scraper.php
+++ web-scraper.php
@@ -1,12 +1,13 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-
 use simplehtmldom\HtmlWeb;
 
 $client = new HtmlWeb();
 
-$file = fopen("./additional-files/scan_the_world_objects.csv", "r");
+# TODO: make this a cli application which takes filename as an option
+$fileName = "./additional-files/scan_the_world_objects.csv";
+$file = fopen($fileName, "r");
 
 while (($line = fgetcsv($file)) !== FALSE) {
     $urls[] = $line[0];
@@ -15,33 +16,30 @@ while (($line = fgetcsv($file)) !== FALSE) {
 fclose($file);
 
 foreach ($urls as $url) {
-
     $html = $client->load($url);
- 
+    
+    if ($html === null) {
+        echo ("Could not download images from url: {$url}\n");
+        continue;
+    }
+    
     $numImagesDownloaded = 0;
     foreach($html->find('img[class=prdimg-gallery]') as $element) {
-       
-
         $imageUrl = $element->src;
         $originalImageUrl = str_replace("/70X70-", "/720X720-", $imageUrl);
+        $downloadedImage = request($originalImageUrl); 
+
         $startImageNamePos = strpos($originalImageUrl, "/720X720-");
         $originalImageName = "720x720-" . substr($originalImageUrl, $startImageNamePos  + 9);
-
-        
-        $downloadedImage = request($originalImageUrl); 
-  
-        $image = "./scan-the-world-images/{$originalImageName}";
+        $imageName = "./scan-the-world-images/{$originalImageName}";
      
-        file_put_contents($image, $downloadedImage); 
+        file_put_contents($imageName, $downloadedImage); 
         $numImagesDownloaded++;
     }
     echo("All images from url: {$url} downloaded. Total images: {$numImagesDownloaded} \n");
 }
 
-
-
-
-
+# TODO: move out to a trait
 function request($url) { 
     $ch = curl_init(); 
   
