91435737b864aa35156e5a5bcfb70dba56c10d8c
assandra <assandrafalls1@gmail.com>
2021-02-09 17:07:27 +0000

    Add more gods to array

--- google-scraper.php
+++ google-scraper.php
@@ -8,68 +8,70 @@ $dotenv->load();
 $apiKey = $_ENV["SERPwOW_API_KEY"];
 
 # Parameters 
-$god = 'hephaestus';
+$gods = ["achlys", "aether", "aion", "ananke", "erebus", "gaia", "hemera", "nemesis", "nesoi", "nyx", "ourea", "phanes", "pontus", "tartarus", "moros", "thanatos"];
 $startPageNum = 1;
 $endPageNum = 5;
 
-foreach (range($startPageNum, $endPageNum) as $pageNum) {
 
-  # Call web scraper API
-  $response = request("https://api.serpwow.com/live/search?api_key={$apiKey}&q={$god}+statue&search_type=images&images_page={$pageNum}");
-  
-  # Save response
-  $responseFile = "./google-images-pages-json/google-{$god}-page-{$pageNum}.json";
-  file_put_contents($responseFile, $response);
+foreach($gods as $god) {
+  foreach (range($startPageNum, $endPageNum) as $pageNum) {
 
-  # Optional - load response from responseFile
-  # $responseFile= "./google-images-pages-json/google-{$god}-page-{$pageNum}.json";
-  # $response = file_get_contents($responseFile);
-
-  $decodedResponse = json_decode($response, true);
-  
-  // TODO throw exception when we dont get anymore results
-
-  # Find all image URLs from response 
-  $images = $decodedResponse["image_results"];
-  
-  foreach ($images as $image) {
-
-    $jpgExtPos = strpos($image["image"], ".jpg");
-    $pngExtPos = strpos($image["image"], ".png");
-
-    if ($jpgExtPos !== false || $pngExtPos !== false) {
-      $imageUrls[] = $image["image"];
-    }
-  }
-
-  # Save image URLs
-  $urlsFileName = "./image-urls/image-urls-{$god}-{$pageNum}.csv";
-  $filePath = fopen($urlsFileName, "w");
-  fputcsv($filePath, $imageUrls, "\n");
-  fclose($filePath);
-
-  # Download images
-  $imageNum = 1; 
-  foreach ($imageUrls as $imageUrl) {
-
-    $downloadedImage = request($imageUrl); 
-
-    $jpgExtPos = strpos($imageUrl, ".jpg");
-    $pngExtPos = strpos($imageUrl, ".png");
+    # Call web scraper API
+    $response = request("https://api.serpwow.com/live/search?api_key={$apiKey}&q={$god}+statue&search_type=images&images_page={$pageNum}");
     
-    if ($pngExtPos !== false) {
-      $file = "./downloaded-images/{$god}-page-{$pageNum}-image-{$imageNum}.png";
-    } else {
-      $file = "./downloaded-images/{$god}-page-{$pageNum}-image-{$imageNum}.jpg";
+    # Save response
+    $responseFile = "./google-images-pages-json/google-{$god}-page-{$pageNum}.json";
+    file_put_contents($responseFile, $response);
+  
+    # Optional - load response from responseFile
+    # $responseFile= "./google-images-pages-json/google-{$god}-page-{$pageNum}.json";
+    # $response = file_get_contents($responseFile);
+  
+    $decodedResponse = json_decode($response, true);
+    
+    // TODO throw exception when we dont get anymore results
+  
+    # Find all image URLs from response 
+    $images = $decodedResponse["image_results"];
+    
+    foreach ($images as $image) {
+  
+      $jpgExtPos = strpos($image["image"], ".jpg");
+      $pngExtPos = strpos($image["image"], ".png");
+  
+      if ($jpgExtPos !== false || $pngExtPos !== false) {
+        $imageUrls[] = $image["image"];
+      }
     }
   
+    # Save image URLs
+    $urlsFileName = "./image-urls/image-urls-{$god}-{$pageNum}.csv";
+    $filePath = fopen($urlsFileName, "w");
+    fputcsv($filePath, $imageUrls, "\n");
+    fclose($filePath);
+  
+    # Download images
+    $imageNum = 1; 
+    foreach ($imageUrls as $imageUrl) {
+  
+      $downloadedImage = request($imageUrl); 
+  
+      $jpgExtPos = strpos($imageUrl, ".jpg");
+      $pngExtPos = strpos($imageUrl, ".png");
       
-    file_put_contents($file, $downloadedImage); 
-    echo "File downloaded!";
-    $imageNum++;
-  }
- }
-
+      if ($pngExtPos !== false) {
+        $file = "./downloaded-images/{$god}-page-{$pageNum}-image-{$imageNum}.png";
+      } else {
+        $file = "./downloaded-images/{$god}-page-{$pageNum}-image-{$imageNum}.jpg";
+      }
+  
+      file_put_contents($file, $downloadedImage); 
+      $imageNum++;
+    }
+    echo "God: {$god}: Google page {$pageNum} scrape completed downloaded!\n";
+   }
+   echo "Scraping God: {$god} completed\n";
+}
 
 function request($url) { 
   $ch = curl_init(); 
