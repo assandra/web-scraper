a362602b27e27ed15af39ec1ec8a27f22f220306
assandra <assandrafalls1@gmail.com>
2021-04-24 15:18:38 +0100

    script to copy files into subfolders

--- move-files.php
+++ move-files.php
@@ -1,8 +1,38 @@
-# Get all file
-$dir = "tag-classified-busts";
-$files = getFiles();
+<?php
 
+# Get all file from source dir
+$sourceDir = "tag-classified-busts";
+$files = getFiles($sourceDir);
 
+echo "Do assertion on number of files retrieved";
+
+# Make target dirs
+$targetDir = "base-dir";
+$numOfDirs = intdiv(count($files),500) + 1;
+
+foreach (range(1, $numOfDirs) as $dirName) {
+    mkdir($base . $dirName);
+}
+
+echo "Do assertion on directories created here";
+
+// Copy files from source to target
+$filesToMove = [];
+$dirName = 1;
+foreach ($file as $files) {
+    $filesToMove[] = $file;
+
+    if (count($filesToMove) === 500) {
+        $source = $sourceDir . $file; 
+        $target = $targetDir . str($dirName) . $file;
+        copy($source, $target);
+
+        $dirName++;
+        $filesToMove = [];
+    }
+}
+    
+echo "Do assertion on images in directories total and final one";
 
 function getFiles($dir) {
     $files = scandir($dir);
